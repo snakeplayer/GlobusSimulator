0cfa90278a1f125510a0b93e09cab33e0bed0be7
snakeplayer <chauche.benoit@gmail.com>
2018-01-31 15:09:29 +0100

    Init
    Creation of the basis.

---
+++ GlobusSimulatorTests/HumanTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GlobusSimulator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GlobusSimulator.Tests
+{
+    [TestClass()]
+    public class HumanTests
+    {
+        [TestMethod()]
+        public void HumanTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
