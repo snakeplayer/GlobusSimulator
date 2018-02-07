59d217d986e0b232010c727e5206ed32a74f505a
Baptiste C <coudray.baptiste@gmail.com>
2018-02-07 15:24:38 +0100

    Created Path class with Unit Tests

--- GlobusSimulator/Path.cs
+++ GlobusSimulator/Path.cs
@@ -1,12 +1,57 @@
-﻿using System;
+﻿/*
+ * Authors : Baptiste COUDRAY, Benoit CHAUCHE
+ * Enterprise : CFPTI, T.IS-E2
+ * Date : 31.01.2018
+ * Project : GlobusSimulator
+ * Project desc. : A simulated Globus shop
+ * Class : Path.cs
+ * Class desc. : Reprensents a path
+ */
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Drawing;
 
 namespace GlobusSimulator
 {
-    class Path
+    public class Path
     {
+        #region Consts
+        #endregion
+        #region Fields
+        #endregion
+        #region Properties
+        public List<Point> Points { get; private set; }
+        #endregion
+        #region Constructors
+        public Path() : this(new List<Point>())
+        {
+            // no code
+        }
+        public Path(List<Point> points)
+        {
+            this.Points = points;
+        }
+        #endregion
+        #region Methods
+        public void AddPoint(Point point)
+        {
+            this.Points.Add(point);
+        }
+
+        public void AddPoints(List<Point> points)
+        {
+            this.Points.AddRange(points);
+        }
+
+        public void RemovePoint(Point point)
+        {
+            this.Points.Remove(point);
+        }
+
+        public void RemovePoints(List<Point> points)
+        {
+            points.ForEach(p => this.Points.Remove(p));
+        }
+        #endregion
     }
 }
---
+++ GlobusSimulatorTests/PathTests.cs
@@ -0,0 +1,85 @@
+﻿/*
+ * Authors : Baptiste COUDRAY, Benoit CHAUCHE
+ * Enterprise : CFPTI, T.IS-E2
+ * Date : 07.02.2018
+ * Project : GlobusSimulator
+ * Project desc. : A simulated Globus shop
+ * Class : PathTests.cs
+ * Class desc. : Represents a path - UNIT TESTS CLASS
+ */
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FluentAssertions;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace GlobusSimulator.Tests
+{
+    [TestClass()]
+    public class PathTests
+    {
+        [TestMethod()]
+        public void PathTest()
+        {
+            Path target = new Path();
+            target.Points.Should().NotBeNull();
+        }
+
+        [TestMethod()]
+        public void PathTest1()
+        {
+            List<Point> points = new List<Point>()
+            {
+                new Point(10, 10)
+            };
+            Path target = new Path(points);
+            target.Points.Should().Contain(points);
+        }
+
+        [TestMethod()]
+        public void AddPointTest()
+        {
+            Point point = new Point(10, 10);
+            Path target = new Path();
+            target.AddPoint(point);
+            target.Points.Should().Contain(point);
+        }
+
+        [TestMethod()]
+        public void AddPointsTest()
+        {
+            List<Point> points = new List<Point>()
+            {
+                new Point(15,15),
+                new Point(30,30)
+            };
+            Path target = new Path();
+            target.AddPoints(points);
+            target.Points.Should().Contain(points);
+        }
+
+        [TestMethod()]
+        public void RemovePointTest()
+        {
+            Point point = new Point(10, 10);
+            Path target = new Path();
+            target.AddPoint(point);
+            target.Points.Should().Contain(point);
+            target.RemovePoint(point);
+            target.Points.Should().NotContain(point);
+        }
+
+        [TestMethod()]
+        public void RemovePointsTest()
+        {
+            List<Point> points = new List<Point>()
+            {
+                new Point(15,15),
+                new Point(30,30)
+            };
+            Path target = new Path();
+            target.AddPoints(points);
+            target.RemovePoints(points);
+            target.Points.Should().NotContain(points);
+        }
+    }
+}
