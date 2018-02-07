16ab7781fcfca2ffc0a0c370dcdec28e0907f38b
BenoitChauche <chauche.benoit@gmail.com>
2018-02-07 12:54:54 +0100

    Human class & unit tests
    Coding of the whole human class and unit tests

--- GlobusSimulator/Human.cs
+++ GlobusSimulator/Human.cs
@@ -10,6 +10,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,26 +21,74 @@ namespace GlobusSimulator
     {
         #region Consts
 
+        private const int DEFAULT_WIDTH = 10;
+        private const int DEFAULT_HEIGHT = 10;
+        private const int DEFAULT_POSITION_X = 0;
+        private const int DEFAULT_POSITION_Y = 0;
+
         #endregion
 
         #region Fields
+
+        private int _numberOfItems;
 
         #endregion
 
         #region Properties
 
+        public Rectangle Shape { get; set; }
+        public int NumberOfItems {
+            get {
+                return _numberOfItems;
+            }
+            set {
+                _numberOfItems = value < 0 ? 0 : value;
+            }
+        }
+
         #endregion
 
         #region Constructors
 
-        public Human()
-        {
+        public Human() : this(DEFAULT_POSITION_X, DEFAULT_POSITION_Y) { }
 
+        public Human(int pPositionX, int pPositionY) : this(new Point(pPositionX, pPositionY)) { }
+
+        public Human(Point pPoint) : this(pPoint, new Size(DEFAULT_WIDTH, DEFAULT_HEIGHT)) { }
+
+        public Human(int pPositionX, int pPositionY, int pSizeWidth, int pSizeHeight) 
+            : this(new Point(pPositionX, pPositionY), new Size(pSizeWidth, pSizeHeight)) { }
+
+        public Human(Point pPoint, Size pSize)
+        {
+            Point p = pPoint;
+            Size s = pSize;
+
+            this.Shape = new Rectangle(p, s);
+            this.NumberOfItems = 0;
         }
 
         #endregion
 
         #region Methods
+
+        public void AddItem()
+        {
+            this.NumberOfItems++;
+        }
+
+        public bool RemoveItem()
+        {
+            bool wasRemoved = false;
+
+            if (this.NumberOfItems > 0)
+            {
+                this.NumberOfItems--;
+                wasRemoved = true;
+            }
+
+            return wasRemoved;
+        }
 
         #endregion
     }
--- GlobusSimulatorTests/HumanTests.cs
+++ GlobusSimulatorTests/HumanTests.cs
@@ -1,20 +1,69 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using GlobusSimulator;
+﻿/*
+ * Authors : Baptiste COUDRAY, Benoit CHAUCHE
+ * Enterprise : CFPTI, T.IS-E2
+ * Date : 07.02.2018
+ * Project : GlobusSimulator
+ * Project desc. : A simulated Globus shop
+ * Class : HumanTests.cs
+ * Class desc. : Reprensents a human being - UNIT TESTS CLASS
+ */
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GlobusSimulator;
+using System.Drawing;
+using FluentAssertions;
 
-namespace GlobusSimulator.Tests
+namespace GlobusSimulatorTests
 {
     [TestClass()]
     public class HumanTests
     {
         [TestMethod()]
-        public void HumanTest()
+        public void HumanCtorTest1()
         {
-            Assert.Fail();
+            Human target = new Human();
+
+            target.Should().NotBeNull();
+            target.Shape.Should().NotBeNull();
+            target.NumberOfItems.Should().Be(0);
+        }
+
+        [TestMethod()]
+        public void HumanAddItemTest1()
+        {
+            Human target = new Human();
+
+            target.AddItem();
+
+            target.NumberOfItems.Should().Be(1);
+        }
+
+        [TestMethod()]
+        public void HumanRemoveItemTest1()
+        {
+            Human target = new Human();
+
+            bool wasRemoved = target.RemoveItem();
+
+            wasRemoved.Should().BeFalse();
+            target.NumberOfItems.Should().Be(0);
+        }
+
+        [TestMethod()]
+        public void HumanRemoveItemTest2()
+        {
+            Human target = new Human();
+
+            target.AddItem();
+            bool wasRemoved = target.RemoveItem();
+
+            wasRemoved.Should().BeTrue();
+            target.NumberOfItems.Should().Be(0);
         }
     }
 }
